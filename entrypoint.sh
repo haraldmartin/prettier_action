3d0e829c293fbfbf3ad5bda83c2899d4869654e7
Martin Ström <name@my-domain.se>
2020-02-24 14:30:31 +0100

    just run the check (dont commit)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,59 +3,5 @@
 # x would be for showing the commands before they are executed
 set -eu
 
-# FUNCTIONS
-# Function for setting up git env in the docker container (copied from https://github.com/stefanzweifel/git-auto-commit-action/blob/master/entrypoint.sh)
-_git_setup ( ) {
-    cat <<- EOF > $HOME/.netrc
-      machine github.com
-      login $GITHUB_ACTOR
-      password $GITHUB_TOKEN
-      machine api.github.com
-      login $GITHUB_ACTOR
-      password $GITHUB_TOKEN
-EOF
-    chmod 600 $HOME/.netrc
-
-    git config --global user.email "actions@github.com"
-    git config --global user.name "GitHub Action"
-}
-
-# Checks if any files are changed
-_git_changed() {
-    [[ -n "$(git status -s)" ]]
-}
-
-# Pushes to the according upstream (origin or input branch)
-_git_push() {
-    if [ -z "$INPUT_BRANCH" ]
-    then
-        git push origin
-    else
-        git push --set-upstream origin "HEAD:$INPUT_BRANCH"
-    fi
-}
-
-# PROGRAM
-echo "Installing prettier..."
 npm install --silent --global prettier
-
-echo "Prettifing files..."
 prettier $INPUT_PRETTIER_OPTIONS
-
-# To keep runtime good, just continue if something was changed
-if _git_changed;
-then
-  # Calling method to configure the git environemnt
-  _git_setup
-  echo "Commiting and pushing changes..."
-  # Switch to the actual branch
-  git checkout $INPUT_BRANCH || echo "Problem checking out the specified branch: $INPUT_BRANCH"
-  # Add changes to git
-  git add "${INPUT_FILE_PATTERN}" || echo "Problem adding your files with pattern ${INPUT_FILE_PATTERN}"
-  # Commit and push changes back
-  git commit -m "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>" ${INPUT_COMMIT_OPTIONS:+"$INPUT_COMMIT_OPTIONS"}
-  _git_push
-  echo "Changes pushed successfully."
-else
-  echo "Nothing to commit. Exiting."
-fi
